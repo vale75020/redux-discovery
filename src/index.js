a47a069e4be9324e15ea50dc48827046f0fe9eb5
vale75020 <valentinosuma@hotmail.com>
2019-05-17 17:08:51 +0200

    setting store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from "redux";
+
+const defaultState = {
+  user: "Valentino",
+  products: ['iPhone']
+};
+
+const stateReducer = (state = defaultState, action) => {  // function link le previousState avec une action
+  if (action.type === "CHANGE_NAME") {  // traiter action
+    return {  // new state
+    //   user: action.payload,
+    //   products: state.products
+      ...defaultState,
+      user: action.payload
+    };
+  }
+  return state;
+};
+
+const changeName = {
+  type: "CHANGE_NAME", // definir action
+  payload: "Matthieu"
+};
+
+const store = createStore(stateReducer); // initialiser le state
+
+console.log(store.getState());
+store.dispatch(changeName); // appeler l'action
+console.log(store.getState());
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
